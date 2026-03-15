08427ae682926189076f6f0c8950558ddf361444
BhoomikaSharma29 <bhoomikash2910@gmail.com>
2026-03-15 09:24:51 +0530

    Add files via upload

---
+++ Assignment 1 Python.py
@@ -0,0 +1,193 @@
+1.
+a="Hello Python"
+print (a)
+
+
+2.
+name = input("Enter a name:")
+print("welcome",name)
+
+
+3.
+num = int(input("Write a number:"))
+print(num)          
+
+
+4.
+num1= int(input("1:"))
+num2= int(input("2:"))
+print(num1)
+print(num2)
+
+
+
+5.
+N = input("Name:")
+A = input("Age:")
+print("My name is",N,"and my age is",A)
+
+
+
+6.
+num = int(input("Enter a number:"))
+sqr = num*num
+print("Square:",sqr)
+
+
+
+7.
+A= int(input("Enter A number:"))
+B= int(input("Enter B number:"))
+C= A+B
+print("Sum of two numbers:",C)
+
+
+
+8.
+print(34+7)
+print(34-7)
+print(34*7)
+
+
+
+9.
+a=34
+print("The number entered is",a)
+
+
+10
+num=45
+print("Double:",num*2)
+
+
+
+11
+V=float(input("Enter a value:"))
+print(V)
+
+
+
+
+12
+a=34.54
+b=56.64
+c=a+b
+print(c)
+
+
+
+
+13.
+a=456.78
+print(a/2)
+
+
+
+
+14.
+Len=float(input("Enter Length:"))
+Wid=float(input("Enter Width:"))
+Area=Len*Wid
+print("Area of rectangle:",Area)
+          
+
+
+
+15.
+r=float(input("Enter Radius:"))
+area=3.14*r*r
+print("Area of circle:",area)
+
+
+
+
+16.
+a=float(input("A:"))
+b=int(input("B:"))
+print(a,b)
+
+
+
+17.
+TM=575
+NOS=7
+print("Average",float(TM/NOS))
+
+
+
+18.
+P=float(499.50)
+Q=int(6)
+print("Total cost:",P*Q)
+
+
+
+
+19.
+a=int(45)
+b=float(34.55)
+print("Sum:",a+b)
+
+
+
+20.
+Salary=float(56778.75)
+Bonus=int(1499)
+print("Final Salary:",Salary+Bonus)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
